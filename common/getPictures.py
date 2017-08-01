f562d164bfcbaf07be0918f0a32cb4fc322dd805
sam.li <sam.li@doohui.com>
2017-08-01 10:51:51 +0800

    提交

--- common/getPictures.py
+++ common/getPictures.py
@@ -2,12 +2,19 @@ import urllib.request
 
 class getPictures():
 
-    headers = ''
+    headers = []
 
-    def __init__(self):
+    def __init__(self, headers):
+        self.headers = headers
         return
 
-    def download(self,imgurl,downloadPath):
-        urllib.request.urlretrieve(imgurl,downloadPath)
+    def download(self, imgurl, downloadPath):
+        # http://www.bubuko.com/infodetail-1871264.html  解决下载文件被403
+        request = urllib.request
+        opener = request.build_opener()
+        opener.addheaders = [self.headers]
+        request.install_opener(opener)
+        request.urlretrieve(imgurl, downloadPath)
 
         print('download success')
+
