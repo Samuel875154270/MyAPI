fc0638419b543fcd670b6de2f83b8c794f5627c8
Samuel <875154270@qq.com>
2017-09-19 21:30:14 +0800

    tt Signed-off-by: Samuel <875154270@qq.com>

--- test20170823.py
+++ test20170823.py
@@ -1,27 +1,25 @@
-# class SchoolMember(object):    # 父类
-#     def __init__(self, name, age):  # 对象建立时马上对此对象初始化
-#         self.name = name
-#         self.age = age
-#         print('initialized SchoolMember is %s' % self.name)     # 注意格式
+import json
+
+# with open('./json.json', 'r') as jsonFile:
+#     data = json.load(jsonFile)
+#     print(data)
 #
-#     def tell(self):     # 又一个方法
-#         print('name: "%s"  age:%s' % (self.name, self.age))     # 格式
-#
-#
-# class Teacher(SchoolMember):  # 子类引用
-#     def __init__(self, name, age, salary):          # 自己的方法添加额外元素
-#         SchoolMember.__init__(self, name, age)      # 此方法里要加入父类方法的引用，很关键的一步
-#         self.salary = salary    # 添加具体内容
-#         print('initialized teacher is %s' % self.name)
-#
-#     def tell(self):
-#         SchoolMember.tell(self)     # 同上很关键
-#         print("salary:%d" % self.salary)
-#
-#
-# teacher = Teacher("li", 23, 6000)      # 调用与验证
-# teacher.tell()
-from testClass import testClass
-x = testClass()
-js = x.ApiService()
-print(js['Appid'])
+
+jsonData = [
+  {
+    "uuid": "8EDBCD47-2F39-4618-BAE1-F1A3910F533A",
+    "password": "abc123"
+  },
+  {
+    "uuid": "8EDBCD47-2F39-4618-BAE1-F1A3910F533A",
+    "password": "abc123"
+  },
+  {
+    "uuid": "中文",
+    "password": "abc123"
+  }
+]
+# json.dump(json_dict, f, indent=4)，
+with open('./json2.json', 'w') as jsonFile2:
+    # jsonFile2.write(json.dumps(jsonData, ensure_ascii=False, indent=4))
+    print(json.dumps(jsonData, ensure_ascii=False, indent=4))
